1847ef6ab3398a661a3978a2ebbefe06aaf900c5
Jeremiasz Major <jrh.mjr@gmail.com>
2025-07-20 22:56:44 +0200

    don't error in projects without svelte

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
+/* eslint-disable import-x/no-mutable-exports */
+
 export { default } from './base.js';
-export { default as svelte } from './svelte.js';
+
+/** @type {import('eslint').Linter.Config[]} */
+let svelte;
+
+// @ts-expect-error
+svelte = 'eslint-plugin-svelte must be installed to import { svelte } from @jrmajor/eslint-config';
+try {
+	await import('eslint-plugin-svelte');
+	svelte = (await import('./svelte.js')).default;
+} catch {
+}
+
+export { svelte };
