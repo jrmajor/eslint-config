0cc66f0e2fd36a5cdf3a0e6e12ed51a20d483b25
Jeremiasz Major <jrh.mjr@gmail.com>
2025-07-31 15:51:56 +0200

    fix unused expressions

--- src/js/ts.js
+++ src/js/ts.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'eslint/config';
 import ts from 'typescript-eslint';
+import { jsConfig } from './js.js';
 
 export const tsConfig = defineConfig({
 	name: 'ts',
@@ -8,9 +9,16 @@ export const tsConfig = defineConfig({
 		ts.configs.recommended,
 	],
 	rules: {
+		// eslint base rule now handles ts syntax
+		'@typescript-eslint/no-unused-expressions': 'off',
+		// @ts-expect-error
+		'no-unused-expressions': jsConfig[1].rules['no-unused-expressions'],
+
+		// disable some defaults
 		'@typescript-eslint/ban-ts-comment': 'off',
 		'@typescript-eslint/no-explicit-any': 'off',
 
+		// custom
 		'@typescript-eslint/array-type': ['warn', { default: 'array-simple', readonly: 'array-simple' }],
 		'@typescript-eslint/consistent-generic-constructors': 'warn',
 		'@typescript-eslint/consistent-indexed-object-style': 'warn',
